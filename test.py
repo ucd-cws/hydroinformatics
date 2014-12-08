9f54da1dfdf1529f24db1e8813e2c5e10163f69b
Kyle Calica <kycalica@gmail.com>
2014-12-08 13:41:42 -0800

    Broke test.py into usable functions and tested it. Only shows graphs and takes dates in an assumed format. will work on django display and then add functionality as i go.

--- test.py
+++ test.py
@@ -5,28 +5,49 @@ import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 
-#take data from csv into pandas data frame object
-data = pd.read_csv("C:\\Users\\kacalica\\Desktop\\test.csv")
 
-#extraction into list 
-tmpx = list(data['timedate'])
-tmpy = list(data['Xvalues'])
-date_range = len(tmpx)
+# csv_file = r"C:\Users\kacalica\Desktop\test.csv" 
+
+def grab_data_csv(csv_file):
 
 
+	#take data from csv into pandas data frame object
+	data = pd.read_csv(csv_file)
 
-# date conversion 
-i = 0
-for date in tmpx : 
-	date = time.strptime(date,"%d-%b-%y" )
-	date = datetime.fromtimestamp(mktime(date))
-	tmpx[i] = date
-	i = i + 1 
-#converts time.struct_time object dates list into list of numbers for plot
-dates = matplotlib.dates.date2num(tmpx)
+	#extraction into list 
+	tmpx = list(data['timedate'])
+	tmpy = list(data['Xvalues'])
+	date_range = len(tmpx)
+	
+	return tmpx, tmpy, date_range 
+		
+	#converts date into usable format
+	#make a more general function that handles any date format 
+def date_conversion(tmpx, date_range): 
+	i = 0
+	for date in tmpx : 
+		date = time.strptime(date,"%d-%b-%y" )
+		date = datetime.fromtimestamp(mktime(date))
+		tmpx[i] = date
+		i = i + 1 
+	#converts time.struct_time object dates list into list of numbers for plot
+	ml_dates = matplotlib.dates.date2num(tmpx)
+	return ml_dates
 
+def graph_data(dates, Yvalues):	
+	plt.plot_date(dates, Yvalues) #plots the data
+	plt.show() #actually shows the data 
+	
+	
+# def main():	 
 
+	# data = grab_data_csv(csv_file) 
+	# dates = date_conversion(data[0], data[2])
+	# print data[1]
+	# print "\n"
+	# print dates
+	# print "\n"
+	# graph_data(dates, data[1])
+	
 
-plt.plot_date(dates, tmpy) #plots the data
-plt.show() #actually shows the data 
-
+# main()
